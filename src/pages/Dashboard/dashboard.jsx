70d48040d03996b781d29ac1f9fde60ee6d1b446
WillianSCorrea <willianwcorrea@gmail.com>
2025-04-02 16:58:50 -0300

    Estrutura De pastas do projeto & Routes e Layout

---
+++ src/pages/Dashboard/dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './dashboard.css'
+const dashboard = () => {
+  return (
+    <>
+      <h1>DashBoard</h1>
+    </>
+  )
+}
+
+export default dashboard
---
+++ src/routes/routes.jsx
@@ -0,0 +1,29 @@
+// src/routes/routes.jsx
+import { Routes, Route } from 'react-router-dom'
+import Home from '../pages/Home/home'
+import Cadastro from '../pages/Cadastro/cadastro'
+import Login from '../pages/Login/login';
+import Dashboard from '../pages/Dashboard/dashboard';
+import AccountLayout from '../layouts/AccountLayout';
+import MainLayout from '../layouts/MainLaoyout';
+export function AppRoutes() {
+  return (
+     <Routes>
+
+       <Route path="/" element={<Home />} />
+    
+     <Route element={<AccountLayout />}>
+       <Route path="/cadastro" element={<Cadastro />} />
+     <Route path="/login" element={<Login />} />
+     </Route>
+
+     <Route element={<MainLayout />}>
+       <Route path="/dashboard" element={<Dashboard />} />
+    {/* <Route path="/Resumo" element={<Resumo />} /> */}
+     </Route>
+     
+
+     
+   </Routes>
+  )
+}

80331fe67776e05b33d77d0ab6a6d81476a29519
Victor <victorsantosflx@gmail.com>
2025-04-08 18:17:33 -0300

    Página Home

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -7,18 +7,23 @@ import './home.css'
 
 
 export default function Home() {
-
-
   return (
-    <div className="home-container">
-     
-      <main>
-        
-          <h1>Bem-vindo ao Sabidos</h1>
-          <li><Link to="/cadastro">Cadastre se agora</Link></li>
-          <li><Link to="/login">Faça login</Link></li>
-        
-      </main>
+    <div className="container">
+      <div className="bloco esquerda">
+        <img src="frame2.svg" className='logo1'/>
+        <p className='texto1'>Organize seus estudos <br /> de forma <span className="sublinhado">fácil</span></p>
+        <button className='botao'>
+          <a>Cadastre-se ! É grátis</a>
+        </button>
+        <a className='sub'><br />Já tem uma conta? Entre !</a>
+      </div>
+      <div className="bloco direita">
+        <p className='texto2'>Trabalhe de qualquer lugar</p>
+        <div className='bloco2'>
+        <img src="logo2.svg" className='logo2'/>
+        <a className='texto3'><br />
+        Mantenha as informações<br></br>importantes à mão; suas<br></br>notas sincronizam<br></br>automaticamente em todos<br></br>os seus dispositivos.</a></div>
+        </div>
     </div>
   )
 }

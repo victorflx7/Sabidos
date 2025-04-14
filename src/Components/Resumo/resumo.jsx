15e5e48ef856a1fcb179f382e8afe4fc0381f149
Victor <victorsantosflx@gmail.com>
2025-04-14 19:42:12 -0300

    Página Resumo-Front Concluída

--- src/Components/Resumo/resumo.jsx
+++ src/Components/Resumo/resumo.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import './resumo.css'
 
 const resumo = () => {
   return (
     <div>
-      
+      <main>
+        <div className='container'>
+          <div className='blocoesquerdo'>
+            <div className='bloquinho'>Aula de Química
+            <p><br></br>04/02</p>
+              </div>
+            <div className='bloquinho'>Aula de Biologia
+            <p><br></br>04/02</p>
+            </div>
+            <div className='bloquinho'>Aula de História
+            <p><br></br>04/02</p>
+            </div>
+            <div className='bloquinho'>Aula de Química
+            <p><br></br>04/02</p>
+              </div>
+              <div className='bloquinho'>Aula de Química
+            <p><br></br>04/02</p>
+              </div>
+              <div className='bloquinho'>Aula de Química
+            <p><br></br>04/02</p>
+              </div>
+              <div className='bloquinho'>Aula de Química
+            <p><br></br>04/02</p>
+              </div>
+          </div>
+        <div className='blocodireito'>
+        <input type="text" className='input' placeholder='Título'/>
+        <input type="text" className='input2' placeholder='Digite aqui a descrição'/>  
+        <button className='botao1'>
+          <img src="485.svg" className='imagem1'/></button>
+        </div>
+        </div>
+      </main>
     </div>
   )
 }
--- src/pages/Resumo/ResumoPage.jsx
+++ src/pages/Resumo/ResumoPage.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import './ResumoPage.css'
 import Header from '../../Components/Header/Header';
+import Resumo from '../../Components/Resumo/resumo';
 import Footer from '../../Components/Footer/Footer';
 
 const ResumoPage = () => {
   return (
     <>
       <Header title='Resumo'/>
-      <main></main>
+      <Resumo></Resumo>
       <Footer></Footer>
     </>
   )

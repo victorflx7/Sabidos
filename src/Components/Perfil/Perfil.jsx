f91b94dcce2b82e4f6e595310d019387daaf77c2
Victor <victorsantosflx@gmail.com>
2025-04-20 22:25:25 -0300

    Estilização da página Perfil

--- src/Components/Perfil/Perfil.jsx
+++ src/Components/Perfil/Perfil.jsx
@@ -8,19 +8,19 @@ const Profile = () => {
       
       <div className="box1">
         <figure>
-          <img src="vite.svg" alt="" className="imgPerfil" />
+          <img src="alex.svg" alt="" className="imgPerfil" />
+          <img src="edit.svg" className='img2'></img>
         </figure>
-         
-        <h1 className="NomePerfil">sjhdfjsydfs</h1>
+        <h1 className="NomePerfil">Alex Cardoso</h1>
       </div>
       <div className="box2">
         <div className="info1">
-          <h3>Usuario</h3>
-          <p>ksdkfjsdjfs</p>
+          <h3 className='user'>Usuário</h3>
+          <p className='user2'>@Alex123</p>
         </div>
         <div className="info2">
-          <h3>Email</h3>
-          <p>fhsdfis@gmail.com</p>
+          <h3 className='email'>Email</h3>
+          <p className='email2'>alexxx@gmail.com</p>
         </div>
       </div>
     </div>

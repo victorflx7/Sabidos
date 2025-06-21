994c5abdef7301a5c8ee33e5f85fdd0ce571b058
Vitaminazz <vitamina.twitu@gmail.com>
2025-06-20 23:51:31 -0300

    React Lazy com Suspense

---
+++ src/Components/Carregando/carregando.jsx
@@ -0,0 +1,12 @@
+import './carregando.css'
+
+const Carregando = () => {
+  return (
+    <div className="container-carregando">
+      <div className="carregando-giro">
+      </div>
+    </div>
+  )
+}
+
+export default Carregando
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,38 +1,45 @@
-// src/routes/routes.jsx
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+
 import PrivateRoute from './privateRoute';
-import Home from '../pages/Home/home'
+
+import Carregando from '../Components/Carregando/carregando';
+
+import Home from '../pages/Home/home';
 import Cadastro from '../pages/CadastroPage/cadastro'
-import Login from '../pages/LoginPage/login';
-import Dashboard from '../pages/Dashboard/dashboard';
-import AccountLayout from '../layouts/AccountLayout';
-import Resumo from "../pages/Resumo/ResumoPage"
-import Agenda from "../pages/Agenda/AgendaPage"
-import Pomodoro from "../pages/Pomodoro/PomodoroPage"
-import Perfil from '../pages/PerfilPage/PerfilPage';
-import Flashcard from '../pages/Flashcard/flashcardPage';
+import Login from '../pages/LoginPage/login'
+
+const Dashboard = lazy(() => import ('../pages/Dashboard/dashboard'));
+const AccountLayout = lazy(() => import ('../layouts/AccountLayout'));
+const Resumo = lazy(() => import ("../pages/Resumo/ResumoPage"));
+const Agenda = lazy(() => import ("../pages/Agenda/AgendaPage"));
+const Pomodoro = lazy(() => import ("../pages/Pomodoro/PomodoroPage"));
+const Perfil = lazy(() => import ('../pages/PerfilPage/PerfilPage'));
+const Flashcard = lazy(() => import ('../pages/Flashcard/flashcardPage'));
 
 export function AppRoutes() {
   return (
-    <Routes>
-      <Route path="/home" element={<Home />} />
-      <Route path="/" element={<Home />} />
-
-      <Route element={<AccountLayout />}>
-        <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="/login" element={<Login />} />
-      </Route>
-
-
-      <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
-      <Route path="/Resumo" element={<PrivateRoute><Resumo /></PrivateRoute>} />
-      <Route path="/Agenda" element={<PrivateRoute><Agenda /></PrivateRoute>} />
-      <Route path="/Pomodoro" element={<PrivateRoute><Pomodoro /></PrivateRoute>} />
-      <Route path="/Perfil" element={<PrivateRoute><Perfil /></PrivateRoute>} />
-      <Route path="/Flashcard" element={<PrivateRoute><Flashcard /></PrivateRoute>} />
-
-
-
-    </Routes>
+    <Suspense fallback={<Carregando />}>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
+  
+        <Route element={<AccountLayout />}>
+          <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
+  
+  
+        <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+        <Route path="/Resumo" element={<PrivateRoute><Resumo /></PrivateRoute>} />
+        <Route path="/Agenda" element={<PrivateRoute><Agenda /></PrivateRoute>} />
+        <Route path="/Pomodoro" element={<PrivateRoute><Pomodoro /></PrivateRoute>} />
+        <Route path="/Perfil" element={<PrivateRoute><Perfil /></PrivateRoute>} />
+        <Route path="/Flashcard" element={<PrivateRoute><Flashcard /></PrivateRoute>} />
+  
+  
+  
+      </Routes>
+    </Suspense>
   )
 }

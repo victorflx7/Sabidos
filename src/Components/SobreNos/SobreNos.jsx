2da169f822059e995ed6750ca1ead9a9f2f81d97
Vitaminazz <vitamina.twitu@gmail.com>
2025-06-23 01:07:50 -0300

    Json com contato e-mail, e txt's

--- src/Components/SobreNos/SobreNos.jsx
+++ src/Components/SobreNos/SobreNos.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import './SobreNos.css'
+import Sobre from '../../data/ContatoSabidos.json'
 
 const SobreNos = () => {
     return (
         <div className='container-snos'>
             <h1 className='titulo-snos'>SabidoS²</h1>
             <p className='descricao-snos'>
-                O SabidoS² é um website interativo que oferece métodos e anotações de estudo gratuitas para alunos do ensino médio e universitário. Nosso objetivo é reunir diversas ferramentas acadêmicas em um único ambiente digital, promovendo uma experiência de aprendizado mais eficiente, organizada e acessível para todos.
+                {Sobre.objetivo}
             </p>
 
             <div className='container-bloco-snos'>
                 <div className='bloco-snos'>
-                    <img className='avatar-snos' src='avatarMatos.svg' alt='' />
+                    <img className='avatar-snos' src='avatarMatos.svg' alt='Imagem-Membro-Matos' />
                     <div className='caixa-snos'>
                         <p className='txt-snos'>Opa, sou o <span className='span-snos'>Victor Matos!</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dignissimos ab omnis.</p>
                     </div>
@@ -20,19 +21,29 @@ const SobreNos = () => {
                     <div className='caixa-snos'>
                         <p className='txt-snos'>Eae, sou o <span className='span-snos'>Luiz!</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dignissimos ab omnis.</p>
                     </div>
-                    <img className='avatar-snos' src='avatarLuiz.svg' alt='' />
+                    <img className='avatar-snos' src='avatarLuiz.svg' alt='Imagem-Membro-Luiz' />
                 </div>
                 <div className='bloco-snos'>
-                    <img className='avatar-snos' src='avatarWill.svg' alt='' />
+                    <img className='avatar-snos' src='avatarWill.svg' alt='Imagem-Membro-Willian' />
                     <div className='caixa-snos'>
-                        <p className='txt-snos'>Ola meu povo, eu sou o <span className='span-snos'>William!</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dignissimos ab omnis.</p>
+                        <p className='txt-snos'>Ola meu povo, eu sou o <span className='span-snos'>Willian!</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dignissimos ab omnis.</p>
                     </div>
                 </div>
                 <div className='bloco-snos'>
                     <div className='caixa-snos'>
                         <p className='txt-snos'>Salve, eu sou o <span className='span-snos'>Victor Freitas!</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dignissimos ab omnis.</p>
                     </div>
-                    <img className='avatar-snos' src='avatarVitas.svg' alt='' />
+                    <img className='avatar-snos' src='avatarVitas.svg' alt='Imagem-Membro-Vitamina' />
+                </div>
+                <div className="bloco-contato">
+                    <p className="texto-contato">
+                        {Sobre.contate}
+                    </p>
+                    <div><img className="icone-contato" src={Sobre.sabidosvg} alt='Icone-SabidoS²-Outline' /></div>
+
+                    <a href={`mailto:${Sobre.email}`} className="email-contato">
+                        {Sobre.email}
+                    </a>
                 </div>
             </div>
         </div>

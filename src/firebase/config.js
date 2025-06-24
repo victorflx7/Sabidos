d9aace9735a390e41091cb040efb2c8e270d9ec7
Victor <victorsantosflx@gmail.com>
2025-06-23 21:03:23 -0300

    Revert "Refactor/lazy routes"

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,5 @@
 import { getAnalytics, logEvent } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 import { getFirestore, collection, addDoc, query, onSnapshot } from "firebase/firestore"; // Importações necessárias para Firestore
 
 const firebaseConfig = {
@@ -18,5 +17,5 @@ const analytics = typeof window !== 'undefined' ? getAnalytics(app) : null;
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app); // Inicializa o Firestore
-const auth = getAuth(app)
-export { db, collection, addDoc, query, onSnapshot, app , analytics, logEvent, auth };
+
+export { db, collection, addDoc, query, onSnapshot, app , analytics, logEvent };
